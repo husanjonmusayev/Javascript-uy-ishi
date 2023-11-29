ce3d15853984bdd94442e153c675e3c351d61df3
husanjonmusayev <husanjonm279@gmail.com>
2023-11-30 01:36:44 +0500

    uy ishi

---
+++ script/script.js
@@ -0,0 +1,77 @@
+// birinchi masala
+// let a =45
+// let b =55
+// let max
+// if(a>b){
+//     max=(a-(a%10))/10
+// }else{
+//     max=(b-(b%10))/10
+// }
+// console.log(max);
+
+// ikkichi masala
+// let a = 258
+// let b = a%100
+// let o = (a-(a%10))/10
+// let y = (a-(a%100))/100
+// let res
+// if(b<o && b<y){
+// res=b
+// }if(o<b && o<y){
+//     res=o
+// }if(y<b && y<o){
+//     res=y
+// }
+// console.log(res);
+
+//uchinchi masala
+// let n =235
+// let res=(n-(n%10))%100/10
+// if(res%3==0){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// to'rtinchi masala
+// let a = 9
+// let b = 5
+// let c = 8
+// if(a>b && a>c){
+//     console.log(a**3);
+// }if(b>a && b>c){
+//     console.log(b**3);
+// }if(c>a && c>b){
+//     console.log(c**3);
+// }
+
+// beshinchi masala
+// let a =1
+// let b =2
+// let c =3
+// let d =4
+// if(a>b && a>c && a>d){
+//     console.log(a);
+// }if(b>a && b>c && b>d){
+//     console.log(b);
+// }if(c>b && c>a && c>d){
+//     console.log(c);
+// }if(d>b && d>c && d>a){
+//     console.log(d);
+// }
+
+// oltinchi masala
+// let a= 4
+// let b= 2
+// let c= 3
+// if(a && a>c ){
+//     console.log(a);
+
+// }if(b<a && b>c ){
+//     console.log(b);
+
+// }if(c<b && c>b ){
+//     console.log(c);
+
+// }
+
